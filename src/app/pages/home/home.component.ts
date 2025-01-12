8fba36cccab696b3a4b1dcc567f8da618a7ddef7
MigueldeNobrega <miguel.eenobrega@gmail.com>
2025-01-12 21:34:58 +0100

    UpdateHomeComponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
 import { Movie } from '../../interfaces/billboard.interface';
+import { SlideshowComponent } from '../../components/slideshow/slideshow.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SlideshowComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
