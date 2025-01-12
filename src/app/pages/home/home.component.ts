81cb222c87be866885a82f2ed3b116710786c35e
MigueldeNobrega <miguel.eenobrega@gmail.com>
2025-01-12 15:45:27 +0100

    HomeComponentResponseGetBillboard

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+
+constructor(private moviesSvc:MoviesService){}
+
+ngOnInit(): void {
+ 
+  this.moviesSvc.getBillboard().subscribe(res=>{
+    console.log(res)
+  })
+
+}
 
 }
