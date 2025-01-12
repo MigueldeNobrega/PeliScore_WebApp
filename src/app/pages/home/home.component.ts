ccefcab8b05e125c8cf02f90e4cd2215cfe67c35
MigueldeNobrega <miguel.eenobrega@gmail.com>
2025-01-12 20:55:48 +0100

    UpdateHomeComponentsLoadMovies

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
+import { Movie } from '../../interfaces/billboard.interface';
 
 @Component({
   selector: 'app-home',
@@ -11,15 +12,25 @@ import { MoviesService } from '../../services/movies.service';
 })
 export class HomeComponent implements OnInit{
 
+  movies:Movie[]=[];
 
 constructor(private moviesSvc:MoviesService){}
 
 ngOnInit(): void {
  
+  this.loadMovies();
+
+}
+
+loadMovies(){
+
   this.moviesSvc.getBillboard().subscribe(res=>{
-    console.log(res)
+
+    this.movies = res;
+    
   })
 
 }
+
 
 }
