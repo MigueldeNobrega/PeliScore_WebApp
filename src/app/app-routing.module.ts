64451022dc0c49a2105b7ad591324dd29e2cfb6e
MigueldeNobrega <miguel.eenobrega@gmail.com>
2025-01-12 14:28:58 +0100

    UpdateRoutingModules.tsPathsAdded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component'
+import { SearchComponent } from './pages/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path:'home', component:HomeComponent},
+{path:'movies/:id', component:MoviesComponent},
+{path:'search/:text', component:SearchComponent },
+
+
+//Si la ruta está vacía o hay un error me redirige a la pantalla Home:
+{path:'', pathMatch:'full', redirectTo:'/home'},
+{path:'**', redirectTo:'/home'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
