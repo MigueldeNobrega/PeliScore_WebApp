3d35aeadb87316174ccaf5c1baa69edfda620102
MigueldeNobrega <miguel.eenobrega@gmail.com>
2025-01-28 21:08:31 +0100

    MoviesPosterCreated

---
+++ src/app/components/movies-poster/movies-poster.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../interfaces/billboard.interface';
+import { CommonModule } from '@angular/common';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-movies-poster',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './movies-poster.component.html',
+  styleUrl: './movies-poster.component.css'
+})
+export class MoviesPosterComponent {
+
+  @Input() movies?:Movie[];
+
+  constructor(private router:Router){}
+
+
+  getStars(voteAvarage:number){
+
+    const starsCount = Math.floor(voteAvarage);
+
+    return Array(starsCount).fill(0);
+
+
+  }
+
+  onMovieClick(movie:Movie){
+
+    this.router.navigate(['/movies',movie.id])
+
+  }
+
+
+}
